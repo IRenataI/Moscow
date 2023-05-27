048dcc79baae52f772298e0f19f0a824c9961fde
Kirill Kuznetsov <kirill2640@yandex.ru>
2023-05-27 10:00:23 +0300

    10.5 minor updates

--- Assets/Scripts/Quests/Shooting-Game/Weapon.cs
+++ Assets/Scripts/Quests/Shooting-Game/Weapon.cs
@@ -6,13 +6,13 @@ public class Weapon : MonoBehaviour
     [Range(0,100)]
     public int Ammo = 5;
     public bool IsWeaponEnable = false;
-    public Camera Parent;
     public GameObject Bullet;
     public Vector3 LocalPosition;
     public int BulletPosition;
     [Range(1f, 15f)]
     public int BulletSpeed = 5;
 
+    private Camera Parent;
     private GameObject __tempBullet;
     private RaycastHit __hit;
     private WeaponAudio _audio;
@@ -26,6 +26,8 @@ public class Weapon : MonoBehaviour
         __questSystem = FindObjectOfType<QuestSystem>();
         __initialPosition = transform.position;
         __boxCollider = GetComponent<BoxCollider>();
+
+        Parent = Camera.main;
     }
     void Update()
     {
@@ -73,18 +75,11 @@ public class Weapon : MonoBehaviour
         Ammo--;
 
         __tempBullet = Instantiate(Bullet);
-        Bullet bullet = __tempBullet.GetComponent<Bullet>();
-
-
-        
+        Bullet bullet = __tempBullet.transform.GetComponentInChildren<Bullet>();        
 
         bullet.SetInitialPosition(transform.position + Parent.transform.forward * BulletPosition);
-        bullet.SetInitialRotation(Quaternion.LookRotation(__hit.point - transform.position));
-        bullet.SetInitialVelocity( (__hit.point - bullet.transform.position).normalized * BulletSpeed); //Camera.main.transform.forward * BulletSpeed);
-
-        //bullet.SetInitialPosition(transform.position + Parent.transform.forward * BulletPosition);
-        //bullet.SetInitialRotation(Quaternion.LookRotation(__hit.point - (transform.position + Parent.transform.forward)));
-        //bullet.SetInitialVelocity( (__hit.point - (transform.position + Parent.transform.forward)).normalized * BulletSpeed); //Camera.main.transform.forward * BulletSpeed);
+        //bullet.SetInitialRotation(Quaternion.LookRotation(__hit.point - transform.position));
+        bullet.SetInitialVelocity( (__hit.point - bullet.transform.position).normalized * BulletSpeed);
     }
     public void SetParent()
     {
@@ -102,3 +97,7 @@ public class Weapon : MonoBehaviour
     }
 }
 //bullet.SetInitialRotation(Quaternion.LookRotation(transform.forward));
+
+//bullet.SetInitialPosition(transform.position + Parent.transform.forward * BulletPosition);
+//bullet.SetInitialRotation(Quaternion.LookRotation(__hit.point - (transform.position + Parent.transform.forward)));
+//bullet.SetInitialVelocity( (__hit.point - (transform.position + Parent.transform.forward)).normalized * BulletSpeed); //Camera.main.transform.forward * BulletSpeed);
