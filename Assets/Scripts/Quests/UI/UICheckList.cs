bb9a969e9f0d6dd73f62d29ef601ce7afdf1a2e8
potikot <91075702+potikot@users.noreply.github.com>
2023-05-28 15:55:14 +0300

    Update: Phone

--- Assets/Scripts/Quests/UI/UICheckList.cs
+++ Assets/Scripts/Quests/UI/UICheckList.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class UICheckList : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI UITasks;
     [SerializeField] private TextMeshProUGUI[] Tasks;
     private QuestSystem __questSystem;
     private void Awake()
     {
         __questSystem = FindObjectOfType<QuestSystem>();
+        UITasks.text = string.Empty;
         for (int i = 0; i < Tasks.Length; i++) 
         {
             Tasks[i].text = "Задача " + (i + 1);
+            UITasks.text += Tasks[i].text + "\n";
         }
     }
     public void UpdateTasks(int index)
     {
         Debug.Log(index);
         if (index > -1 && !__questSystem.GetCurrentQuest.IsQuestCompleted)
+        {
             Tasks[index].text += "(завершено)";//(index + 1) + "(завершено)";
+            UpdateUITasks();
+        }
         else
             throw new NullReferenceException("index < 0");
+    }
+
+    private void UpdateUITasks()
+    {
+        UITasks.text = string.Empty;
+
+        for (int i = 0; i < Tasks.Length; i++)
+        {
+            UITasks.text += Tasks[i].text + "\n";
+        }
     }
 }
