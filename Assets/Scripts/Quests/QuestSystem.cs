e0c2e9382dcc2d6c881887cf48a20cc0e911be7f
potikot <91075702+potikot@users.noreply.github.com>
2023-06-17 19:18:02 +0300

    fix3.2

--- Assets/Scripts/Quests/QuestSystem.cs
+++ Assets/Scripts/Quests/QuestSystem.cs
@@ -19,6 +19,8 @@ public class QuestSystem : MonoBehaviour
         } 
     }
     public bool[] GetMainQuests { get { return __mainQuests; } }
+    public static int GetMainQuestsLength => MAINQUESTLENGTH;
+
     private void Awake()
     {
         __checkList = FindObjectOfType<UICheckList>();
