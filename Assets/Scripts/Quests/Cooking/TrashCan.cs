b9438d4cc1f32acbcc8fc361fec1389d2aca034f
Kirill <k.d.kuznetsov@stud.mospolytech.ru>
2023-06-28 13:53:11 +0300

    28.8 minor script updates

--- Assets/Scripts/Quests/Cooking/TrashCan.cs
+++ Assets/Scripts/Quests/Cooking/TrashCan.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 public class TrashCan : CookingTool
 {
     private void Update()
--- Assets/Scripts/Quests/QuestInteractable.cs
+++ Assets/Scripts/Quests/QuestInteractable.cs
@@ -1,28 +1,18 @@
-using UnityEngine;
-
 public class QuestInteractable : Interactable
 {
-    private ButtonToPressUI __buttonToPress;
-    private Canvas __buttonToPressCanvas;
-    private void Awake()
-    {
-        __buttonToPress = FindObjectOfType<ButtonToPressUI>();
-        __buttonToPressCanvas = __buttonToPress.GetComponent<Canvas>();
-        __buttonToPressCanvas.enabled = false;
-    }
     public override void Select() 
     {
         base.Select();
-        __buttonToPressCanvas.enabled = true;
+        ButtonToPressUI.EnableCanvas();
     }
     public override void Interact()
     {
         base.Interact();
-        __buttonToPressCanvas.enabled = false;
+        ButtonToPressUI.DisableCanvas();
     }
     public override void Deselect()
     {
         base.Deselect();
-        __buttonToPressCanvas.enabled = false;
+        ButtonToPressUI.DisableCanvas();
     }
 }
